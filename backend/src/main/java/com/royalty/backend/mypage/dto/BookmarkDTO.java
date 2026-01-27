182db1d0ee0eb9dace367d0133be2b7717d1bb4b
BOOK-L0DKHG7KP0\ywk59 <ywk5974@gmail.com>
2026-01-27 10:51:13 +0900

    feat: 코드 수정

--- backend/src/main/java/com/royalty/backend/mypage/dto/BookmarkDTO.java
+++ backend/src/main/java/com/royalty/backend/mypage/dto/BookmarkDTO.java
@@ -13,7 +13,7 @@ public class BookmarkDTO {
     private String applicationNumber; // 출원번호
     private String category;       // 분류
     private String applicant;      // 출원인
-    // private String status;      // (추후 데이터 생기면 주석 해제)
+    private String status;      // (추후 데이터 생기면 주석 해제)
     
     private LocalDateTime createdAt; // 북마크한 시간
 }
