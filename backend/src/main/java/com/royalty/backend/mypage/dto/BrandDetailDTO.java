1385de6ac22ef253621b20815ef31f69f9bde6c1
BOOK-L0DKHG7KP0\ywk59 <ywk5974@gmail.com>
2026-01-29 13:49:39 +0900

    chore: update .gitignore rules

--- backend/src/main/java/com/royalty/backend/mypage/dto/BrandDetailDTO.java
+++ backend/src/main/java/com/royalty/backend/mypage/dto/BrandDetailDTO.java
@@ -12,7 +12,7 @@ public class BrandDetailDTO {
     private String description;
     private String currentLogoPath;
     private boolean isNotificationEnabled; // 상세에서도 스위치 조작 가능
-    
+    private LocalDateTime createdAt;
     // 2. 포함된 리스트 정보
     private List<BrandHistoryDTO> historyList; // 로고 변경 이력
 //    private List<ReportDTO> reportList;        // 분석 리포트 목록
